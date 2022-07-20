b496ab1f502342d401370797be2599db479d8b0c
Nikita <Whiteroog@gmail.com>
2022-07-20 21:30:50 +0300

    add style and grid for index file

--- components/header.php
+++ components/header.php
@@ -9,15 +9,27 @@
 подобрать параметры вашего заказа">
 </head>
 <body>
-<header>
-    ООО «ЛесРегионСервис»<br>
-    <span class="words-header">Леса и вышки</span><br>
-
-    +7 (925) 184-50-95<br>
-    <strong>Офис:</strong><br>
-    141801, Московская область, Дмитровский район, город Дмитров, улица Профессиональная, дом № 130, помещение 3<br>
-    <strong>Адрес производства:</strong><br>
-    Московская область, Дмитровский р-н г. Яхрома ул. Ленина 42
+<header class="two-columns-equally">
+    <div class="two-columns-equally left-align color-white-background-blue">
+        <div>
+            ООО «ЛесРегионСервис»<br>
+            <span class="words-header">Леса и вышки</span>
+        </div>
+        <div class="right-align">
+            <span class="words-header color-black-background-yellow">Качество и надежность</span>
+        </div>
+    </div>
+    <div class="two-columns-equally color-black-background-yellow">
+        <div class="center-align">
+            +7 (925) 184-50-95
+        </div>
+        <div class="left-align">
+            <strong>Офис:</strong><br>
+            141801, Московская область, Дмитровский район, город Дмитров, улица Профессиональная, дом № 130, помещение 3<br>
+            <strong>Адрес производства:</strong><br>
+            Московская область, Дмитровский р-н г. Яхрома ул. Ленина 42
+        </div>
+    </div>
 </header>
 <nav>
     <a href="index.php">Главная</a>
